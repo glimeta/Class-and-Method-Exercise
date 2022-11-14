9fa7a4e177d0f1acb9bb5d36c8f134728807137e
glimeta <117176990+glimeta@users.noreply.github.com>
2022-11-14 16:54:48 +0100

    Add project files.

---
+++ Class and Method Exercise/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_and_Method_Exercise
+{
+    class Book
+    {
+        public string Author { get; set; }
+        public string Name   { get; set; }
+        public int    Pages  { get; set; }
+
+        public string Print()
+        {
+            return this.Author + ", " + this.Name + ", " + this.Pages + " pages";
+        }
+
+
+        public Book(string title, string author, int pages)     //constructor to minimize no. of code lines in program 
+        {
+            Author = author;
+            Name =   title;
+            Pages =  pages;
+        }
+
+    }
+}
---
+++ Class and Method Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using Class_and_Method_Exercise;
+using System.Data;
+
+Console.WriteLine("Class and Method Coding Exercises");
+
+List<Book> books = new List<Book>();      //Creates a list of books
+Console.WriteLine("List of Books");
+Console.WriteLine("--------------------------------");
+
+Book book1 = new Book("Alchemist", "Paolo Coelo", 163);
+Book book2 = new Book("Harry Potter", "JKRowling", 400);
+Book book3 = new Book("HThe man who sold his Ferrri", "Robin Sharma", 198);
+Book book4 = new Book("The Purpose Driven Life", "Rick Warren", 368);
+
+Book book = new Book();                //Adds items in the list 
+books.Add(book);
+
+Console.WriteLine(books.Print());   //Prints out the list of books
+
+
+Console.ReadLine();
+
+
